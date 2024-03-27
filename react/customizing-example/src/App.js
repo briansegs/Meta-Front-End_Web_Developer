c38d16390ffa64b7fbc53ce256f0a3fea2c10acd
bsegers <briansegs@gmail.com>
2024-03-27 16:12:36 -0400

    add: Import components

--- react/customizing-example/src/App.js
+++ react/customizing-example/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Nav, Promo, Footer, Intro1, Intro2, Intro3 } from './components/index';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Promo />
+      <Intro1 />
+      <Intro2 />
+      <Intro3 />
+      <Footer />
     </div>
   );
 }
