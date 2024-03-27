5fbdccef58abff82d1ed5612ca7f23340444225a
bsegers <briansegs@gmail.com>
2024-03-27 16:07:50 -0400

    add: Export file

---
+++ react/customizing-example/src/components/index.js
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import Intro1 from "./Intro1";
+import Intro2 from "./Intro2";
+import Intro3 from "./Intro3";
+import Nav from "./Nav";
+import Promo from "./Promo";
+
+export {
+    Footer,
+    Intro1,
+    Intro2,
+    Intro3,
+    Nav,
+    Promo
+}
