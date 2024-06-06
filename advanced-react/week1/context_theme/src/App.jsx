ea91b04c435e169ea05a953477960e318c6712f0
bsegers <briansegs@gmail.com>
2024-06-06 15:49:44 -0400

    feat: Add content and styles

--- advanced-react/week1/context_theme/src/App.jsx
+++ advanced-react/week1/context_theme/src/App.jsx
@@ -1,13 +1,42 @@
 import "./App.css";
 import Switch from "./Switch";
 
+const Title = ({ children }) => {
+  return <h2>{children}</h2>;
+};
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Title>Little Lemon 🍕</Title>
+      <Switch />
+    </div>
+  );
+};
+
+const Paragraph = ({ children }) => {
+  return <p>{children}</p>;
+};
+
+const Content = () => {
+  return (
+    <div className="content">
+      <Title>When it comes to dough</Title>
+      <Paragraph>
+        We are a pizza loving family. And for years, I searched and searched and
+        searched for the perfect pizza dough recipe. I tried dozens, or more.
+        And while some were good, none of them were that recipe that would make
+        me stop trying all of the others.
+      </Paragraph>
+    </div>
+  );
+};
+
 function App() {
   return (
-    <div>
-      <header>
-        <h2>Little Lemon 🍕</h2>
-        <Switch />
-      </header>
+    <div className="app">
+      <Header />
+      <Content />
     </div>
   );
 }
